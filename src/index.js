fffd8b304962bc6598fa06de665ebc7bcdac06aa
Aniket <aniketupatel1@gmail.com>
2019-06-26 18:23:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const container = document.querySelector('#tasks')
+  const taskForm = document.querySelector('#create-task-form')
+
+  taskForm.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const textField = e.target.querySelector('#new-task-description')
+    const text = textField.value
+    const newLi = document.createElement('li')
+    const deleteButton = document.createElement('button')
+    deleteButton.innerHTML = 'X'
+    newLi.innerText = text
+    container.appendChild(newLi)
+    newLi.appendChild(deleteButton)
+  })
+  container.addEventListener('click', function (e) {
+    if (e.target.innerHTML === 'X'){
+      e.target.parentElement.remove()
+    }
+      
+// I need tro create a li
+// I need to change the innertext of that li
+//     
+    // }
+  })
+
 });
